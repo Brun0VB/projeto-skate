dec401f8fa2ec06951e0979b81684f1de2af5937
Bruno Vilela <brunovilela2002@hotmail.com>
2022-10-11 01:17:09 -0300

    página de vendas completa

--- src/app/show-room/show-room.page.ts
+++ src/app/show-room/show-room.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-
+import { CrudService } from '../shared/crud-service';
+import { Skate } from '../shared/skt';
+import { getStorage, ref as refS, getDownloadURL } from "firebase/storage";
+import { Router } from '@angular/router';
+import { IonicSafeString } from '@ionic/angular';
 @Component({
   selector: 'app-show-room',
   templateUrl: './show-room.page.html',
@@ -9,37 +13,70 @@ import { AlertController } from '@ionic/angular';
 export class ShowRoomPage implements OnInit {
 
   produtos: any[] = [];
+  skateImg: String[];
+  p: number = 1;
+  esconderSkateNulo: boolean = false;
+  noData: boolean = false;
+  preLoader: boolean = true;
   
-  constructor(private alertController: AlertController) {
-    this.add();
-  }
+  constructor(
+    private alertController: AlertController,
+    public crudApi: CrudService, 
+    public router: Router,
+    ) {}
 
   ngOnInit() {
+    const storage = getStorage();
+    this.dataState();
+    let s = this.crudApi.GetSkateList();
+    s.snapshotChanges().subscribe(data => {
+      this.produtos = [];
+      this.skateImg = [];
+      data.forEach(item => {
+        let a = item.payload.toJSON();
+        a['$key'] = item.key;
+        getDownloadURL(refS(storage, 'Skate/'+item.key))
+        .then((url) => {
+          this.skateImg[item.key] = url;
+        })
+        this.produtos.push(a as Skate);
+      })
+    })
+  }
+
+  dataState() {
+    this.crudApi.GetSkateList().valueChanges().subscribe(data => {
+      this.preLoader = false;
+      if (data.length <= 0) {
+        this.esconderSkateNulo = false;
+        this.noData = true;
+      } else {
+        this.esconderSkateNulo = true;
+        this.noData = false;
+      }
+    })
   }
 
   async showPopUp(produto){
+
     console.log("evento Ok")
     const alert = await this.alertController.create({
-      subHeader: produto.categoria,
-      message: `<img src=produto.foto` + produto.descricao + produto.preco ,  //fonte da imagem do produto d 
+      header: produto.nome,
+      message: `<img src=${this.skateImg[produto.$key]}>${produto.tamanho}<br> ${produto.lixa}<br> ${produto.material}<br> ${produto.modelo}<br> ${produto.marca}<br> ${produto.linha}<br> ${produto.resina}<br><ion-text color="success">Valor: R$${produto.valor}</ion-text>`, 
       buttons: [
         {
-        text: 'CANCELAR',
-        role: 'cancel',
+          text: 'CANCELAR',
+          role: 'cancel',
         },
         {
-            text:'COMPRAR',
+          text:'COMPRAR',//comprar não faz nada
         },
       ],
     });
-
     await alert.present();
   }
 
-  async add(){
-
-    let produto = {categoria: 'skate',nome:'skt1',descricao:'descrição',preco:'100',foto:"../../assets/images/skate.jpg"};
-
-    this.produtos.push(produto);
+  navega(){
+    this.router.navigate(['home']);
   }
 }
